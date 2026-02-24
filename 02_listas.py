1fdeeb533d8d31d9a496f20e8863666b8b1dabc1
Fausto Cintra <professor@faustocintra.com.br>
2026-02-24 20:22:41 -0300

    (24/02) Continuação de listas

--- 02_listas.py
+++ 02_listas.py
@@ -27,6 +27,50 @@ for elemento in frutas:
 
 print('--------------------------------------')
 
-# Percorrendo a lista de primos e exibindo o quadrado de cada um
+# Percorrendo a lista de primos e exibindo o quadrado de cada um 
 for n in primos:
     print(n ** 2)
+
+# Traço separador (80 vezes o caractere "-")
+print(80 * "-")
+
+# 2) INSERÇÃO DE UM NOVO ELEMENTO NA LISTA
+print("Lista de frutas, ANTES DA INSERÇÃO:", frutas)
+print("Lista de números, ANTES DA INSERÇÃO:", primos)
+
+# Inserindo um novo elemento ao FINAL de uma lista: append()
+frutas.append("maracujá")
+primos.append(37)
+
+print("Lista de frutas, APÓS A INSERÇÃO:", frutas)
+print("Lista de números, APÓS A INSERÇÃO:", primos)
+
+print(80 * "-")
+
+# Inserindo um novo elemento EM UMA POSIÇÃO ESPECÍFICA: insert()
+# Parâmetros:
+# 1º ~> a posição na qual será feita a inserção (obs.: a CONTAGEM
+#       do número das posições COMEÇA EM ZERO)
+# 2º ~> o novo elemento a ser inserido
+
+# Inserindo um novo elemento na PRIMEIRA POSIÇÃO
+frutas.insert(0, "melancia")
+print("Lista de frutas após inserir 'melancia' na primeira posição:")
+print(frutas)
+
+print(80 * "-")
+
+# Inserindo um elemento na QUARTA posição (posição 3)
+frutas.insert(3, "amora")
+print("Lista de frutas após inserir 'amora' na posição 3 (QUARTA posição:)")
+print(frutas)
+
+print(80 * "-")
+
+# 3) ACESSANDO OS VALORES DA LISTA POR SUA POSIÇÃO
+print("Elemento da QUINTA posição:", frutas[4])
+print("Elemento da PRIMEIRA posição:", frutas[0])
+print("Elemento da ÚLTIMA posição:", frutas[-1])
+print("Elemento da PENÚLTIMA posição:", frutas[-2])
+
+
