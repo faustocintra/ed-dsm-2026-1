07ef3960f359c9543c4d98d08bf2481d8a849353
Fausto Cintra <professor@faustocintra.com.br>
2026-03-11 20:26:16 -0300

    (11/03) Término da busca binária

--- 06_busca_binaria.py
+++ 06_busca_binaria.py
@@ -1,3 +1,6 @@
+# Variável global para contar o número de comparações
+comps = None
+
 def busca_binaria(lista, val):
     """
     ALGORITMO DE BUSCA BINÁRIA
@@ -10,10 +13,15 @@ def busca_binaria(lista, val):
     busca não consta da lista buscada.
     """
 
+    global comps    # Usa a variável global dentro da função
+    comps = 0
+
     ini = 0                 # Posição inicial da lista
     fim = len(lista) - 1    # Posição final da lista
 
     while ini <= fim:
+        comps += 1
+
         # Calculando a posição do meio da lista
         # O operador // em Python significa divisão inteira
         # (descarta a parte fracionária, se houver)
@@ -46,3 +54,30 @@ def busca_binaria(lista, val):
     return -1
 
 #############################################################################
+
+# TESTE COM 1M+ DE NOMES
+
+import sys
+sys.dont_write_bytecode = True  # Impede a criação do cache
+
+from time import time
+
+from data.nomes_completos import nomes
+
+buscas = [
+    "EDSON PEREIRA",
+    "MARIA FERREIRA",
+    "VALDIR SILVA",
+    "GILCINEIA GARCIA"
+]
+
+for n in buscas:
+    hora_ini = time()
+    pos = busca_binaria(nomes, n)
+    hora_fim = time()
+    if pos >= 0:
+        print(f"Nome {n} encontrado na posição {pos}.")
+    else:
+        print(f"Nome {n} não encontrado, busca retornou {pos}.")
+    print(f"Número de comparações: {comps}")
+    print(f"Tempo gasto: {(hora_fim - hora_ini) * 1000}ms.\n")
