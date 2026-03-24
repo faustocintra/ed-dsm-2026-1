998c0648cd3e9f782d75cadab8b916a22e4b7ccc
Fausto Cintra <professor@faustocintra.com.br>
2026-03-24 20:21:52 -0300

    (24/03) Continuação de Selection Sort

--- 08_selection_sort.py
+++ 08_selection_sort.py
@@ -1,3 +1,6 @@
+# Variáveis globais de estatística
+comps = trocas = passd = None
+
 def selection_sort(lista):
     """
     ALGORITMO DE ORDENAÇÃO SELECTION SORT
@@ -11,11 +14,16 @@ def selection_sort(lista):
     elemento da lista seja comparado com o último e feita
     a troca entre eles, caso preciso.
     """
+    global comps, trocas, passd
+    comps = trocas = passd = 0
+
     # Loop que vai da primeira até a PENÚLTIMA posição
     # para selecionar o elemento que será comparado com os
     # demais. Este laço também determina a quantidade de
     # passadas que serão executadas (número constante: n-1)
     for pos_sel in range(len(lista) - 1):
+        passd += 1
+
         # Inicia supondo que a posição do menor valor do resto
         # da lista é aquela imediatemente seguinte à do valor
         # selecionado
@@ -27,6 +35,7 @@ def selection_sort(lista):
             # Se o valor que estiver na posição atual (pos) for
             # MENOR do que aquele apontado por pos_menor, ajusta
             # pos_menor para o mesmo valor que pos
+            comps += 1
             if lista[pos] < lista[pos_menor]: pos_menor = pos
 
         # <~ CUIDADO COM A INDENTAÇÃO AQUI!
@@ -35,13 +44,24 @@ def selection_sort(lista):
         # Comparamos os valores das posições pos_menor e pos_sel.
         # Se o PRIMEIRO for MENOR do que o SEGUNDO, efetuamos a
         # troca entre eles
+        comps += 1
         if lista[pos_menor] < lista[pos_sel]:
             lista[pos_sel], lista[pos_menor] = lista[pos_menor], lista[pos_sel]
+            trocas += 1
 
 ################################################################################
 
 # Caso médio
-nums = [7, 0, 6, 8, 1, 3, 9, 4, 2, 5]
+# nums = [7, 0, 6, 8, 1, 3, 9, 4, 2, 5]
+# Comparações: 45; trocas: 8; passadas: 9
+
+# Melhor caso
+# nums = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+# Comparações: 45; trocas: 0; passadas: 9
+
+# Pior caso
+nums = [9, 0, 1, 2, 3, 4, 5, 6, 7, 8]
+# Comparações: 45; trocas: 9; passadas: 9
 
 print("ANTES: ", nums)
 
@@ -49,3 +69,4 @@ print("ANTES: ", nums)
 selection_sort(nums)
 
 print("DEPOIS:", nums)
+print(f"Comparações: {comps}; trocas: {trocas}; passadas: {passd}")
