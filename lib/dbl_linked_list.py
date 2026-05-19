7d62dac6f69a4cd88ebc05fb18df3dd95c12d85f
Fausto Cintra <professor@faustocintra.com.br>
2026-05-19 20:13:39 -0300

    (19/05) Continuação da inserção na lista duplamente encadeada

--- lib/dbl_linked_list.py
+++ lib/dbl_linked_list.py
@@ -48,6 +48,49 @@ class DblLinkedList:
             self.__head = new
             self.__tail = new
 
+        # 2º caso: inserção no início da lista (posição 0)
+        elif pos == 0:
+            new.next = self.__head
+            self.__head.prev = new
+            self.__head = new
+
+        # 3º caso: inserção no final da lista
+        # OBS.: sempre que a posição passada para inserção for
+        # >= count, interpretaremos como inserção no final
+        elif pos >= self.__count:
+            new.prev = self.__tail
+            self.__tail.next = new
+            self.__tail = new
+
+        # 4º caso: inserção em posição intermediária
+        # Esse caso depende de encontrarmos o elemento que atualmente 
+        # está na posição desejada. Isso é feito por um outro método,
+        # __find_node(). 
+
         # Incrementa o número de nodos da lista
         self.__count += 1
+
+    def __find_node(self, pos):
+        """ Método PRIVADO para encontrar um nodo na posição dada """
+        # 1º caso: posição 0, retorna self.__head
+        if pos == 0: return self.__head
+
+        # 2º caso: posição final (self.__count - 1), retorna self.__tail
+        elif pos == self.__count - 1: return self.__tail
+
+        # 3º caso: posição intermediária
+
+        # Se 'pos' estiver na primeira metade da lista (< self.__count // 2),
+        # faz o percurso de busca a partir de self.__head
+        if pos < self.__count // 2:
+            node = self.__head
+            for _ in range(1, pos + 1): node = node.next
+
+        # Senão, estando a posição na segunda metade da lista, faz o
+        # percurso reverso a partir de self.__tail
+        else:
+            node = self.__tail
+            for _ in range(self.__count - 1, pos, -1): node = node.prev
+
+        return node
     
